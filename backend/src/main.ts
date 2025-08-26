5588fa8fc5918c159300e7197be006d799a175fe
pankhur <negipankhur@gmail.com>
2025-08-26 17:27:25 +0530

    pwa fix

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,14 +6,14 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:4200',
+    origin: ['http://localhost:4200', 'http://localhost:8080'],
     credentials: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   });
 
   app.useGlobalPipes(new ValidationPipe());
 
-  
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
